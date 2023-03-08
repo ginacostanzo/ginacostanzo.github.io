ff255f06afa97b4a6ba32d78b8e57dfcabfbebc8
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-03-07 20:22:43 -0500

    Restructure webpage grids

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
+window.onscroll = function() {
+    let jumpBtn = document.querySelector('#jump-to-top');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      jumpBtn.style.display = "block";
+    } else {
+      jumpBtn.style.display = "none";
+    }
+  }
+
 const projects = document.getElementById('projects');
 const resume = document.getElementById('resume');
 const about = document.getElementById('about');
-about.hidden = false;
-projects.hidden = true;
-resume.hidden = true;
+about.style.display = "grid";
+projects.style.display = "none";
+resume.style.display = "none";
 
 document.getElementById('projects-link').addEventListener("click", function(){
-    projects.hidden = false;
-    about.hidden = true;
-    resume.hidden = true;
+    projects.style.display = "block";
+    document.getElementById('projects-link').className = "active";
+    about.style.display = "none";
+    document.getElementById('about-link').className = "inactive";
+    resume.style.display = "none";
+    document.getElementById('resume-link').className = "inactive";
 });
 
 document.getElementById('about-link').addEventListener("click", function(){
-    projects.hidden = true;
-    about.hidden = false;
-    resume.hidden = true;
+    projects.style.display = "none";
+    document.getElementById('projects-link').className = "inactive";
+    about.style.display = "grid";
+    document.getElementById('about-link').className = "active";
+    resume.style.display = "none";
+    document.getElementById('resume-link').className = "inactive";
 });
 
 document.getElementById('resume-link').addEventListener("click", function(){
-    projects.hidden = true;
-    about.hidden = true;
-    resume.hidden = false;
+    projects.style.display = "none";
+    document.getElementById('projects-link').className = "inactive";
+    about.style.display = "none";
+    document.getElementById('about-link').className = "inactive";
+    resume.style.display = "block";
+    document.getElementById('resume-link').className = "active";
 });
