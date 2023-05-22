37dab7ab4f50586f19cb6fec2b706209680b2d6b
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-05-21 21:02:18 -0400

    Remove jump to top button since navigation is now fixed in place

--- index.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-    let jumpBtn = document.querySelector('#jump-to-top');
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      jumpBtn.style.display = "block";
-    } else {
-      jumpBtn.style.display = "none";
-    }
-  }
