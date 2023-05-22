340532af05a787e82fca79884143b93ba6c44245
ginacostanzo <106881838+ginacostanzo@users.noreply.github.com>
2023-05-21 20:13:03 -0400

    Remove js code for toggling page views

--- index.js
+++ index.js
@@ -6,37 +6,3 @@ window.onscroll = function() {
       jumpBtn.style.display = "none";
     }
   }
-
-const projects = document.getElementById('projects');
-const resume = document.getElementById('resume');
-const about = document.getElementById('about');
-about.style.display = "grid";
-projects.style.display = "none";
-resume.style.display = "none";
-
-document.getElementById('projects-link').addEventListener("click", function(){
-    projects.style.display = "block";
-    document.getElementById('projects-link').className = "active";
-    about.style.display = "none";
-    document.getElementById('about-link').className = "inactive";
-    resume.style.display = "none";
-    document.getElementById('resume-link').className = "inactive";
-});
-
-document.getElementById('about-link').addEventListener("click", function(){
-    projects.style.display = "none";
-    document.getElementById('projects-link').className = "inactive";
-    about.style.display = "grid";
-    document.getElementById('about-link').className = "active";
-    resume.style.display = "none";
-    document.getElementById('resume-link').className = "inactive";
-});
-
-document.getElementById('resume-link').addEventListener("click", function(){
-    projects.style.display = "none";
-    document.getElementById('projects-link').className = "inactive";
-    about.style.display = "none";
-    document.getElementById('about-link').className = "inactive";
-    resume.style.display = "block";
-    document.getElementById('resume-link').className = "active";
-});
